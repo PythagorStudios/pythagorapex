e182607ce90e657277a8dfb2f75ac259f6696786
Daniel Sun <danielcsun@hotmail.com>
2016-07-03 14:19:33 -0700

    Fixed code formatting.
    FINN LEARN HOW TO INDENT PLZZZZZ UR KILLIN ME

--- public/javascript/engine-main.js
+++ public/javascript/engine-main.js
@@ -2,28 +2,28 @@
  * Created by finnb on 7/2/16.
  */
 
-    var frameTask;
-    if (window.webkitRequestAnimationFrame) {
-        frameTask = function(cb) {
-            var _cb = function() { cb(); webkitRequestAnimationFrame(_cb); }
-            _cb();
-        };
-    } else if (window.requestAnimationFrame) {
-        frameTask = function(cb) {
-            var _cb = function() { cb(); requestAnimationFrame(_cb); }
-            _cb();
-        };
-    } else if (window.mozRequestAnimationFrame) {
-        frameTask = function(cb) {
-            var _cb = function() { cb(); mozRequestAnimationFrame(_cb); }
-            _cb();
-        };
-    } else {
-        frameTask = function(cb) {
-            setInterval(cb, 1000 / 60);
-        }
+var frameTask;
+if (window.webkitRequestAnimationFrame) {
+    frameTask = function(cb) {
+        var _cb = function() { cb(); webkitRequestAnimationFrame(_cb); }
+        _cb();
+    };
+} else if (window.requestAnimationFrame) {
+    frameTask = function(cb) {
+        var _cb = function() { cb(); requestAnimationFrame(_cb); }
+        _cb();
+    };
+} else if (window.mozRequestAnimationFrame) {
+    frameTask = function(cb) {
+        var _cb = function() { cb(); mozRequestAnimationFrame(_cb); }
+        _cb();
+    };
+} else {
+    frameTask = function(cb) {
+        setInterval(cb, 1000 / 60);
     }
-    window.frameTask = frameTask;
+}
+window.frameTask = frameTask;
 
 window.onload = function() {
     Game.init(); //Whatever game is loaded in the DOM will be initialized
