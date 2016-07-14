c2beee6197ffdae902c1c69d5f533c9f433b725e
Finn Bear <FinnBear@MacLaptop.Bear2Router>
2016-07-13 20:49:17 -0700

    Decreased shape rotation speed.

--- public/javascript/apex-game.js
+++ public/javascript/apex-game.js
@@ -197,7 +197,7 @@ Game.update = function() {
         }
         pys[i] += pyvs[i];
         this.shapes[i].position.z = pys[i];
-        this.shapes[i].rotation.y += 5;
+        this.shapes[i].rotation.y += 1;
     }
 };
 
